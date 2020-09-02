7f1a589dac2c99aaf28df80b46e493144ed5cd12
h5avlov <h5avlov@abv.bg>
2020-09-02 15:56:49 +0300

    Scroll-to-top functionality added

--- assets/js/sell-points.js
+++ assets/js/sell-points.js
@@ -1,6 +1,6 @@
 ( function() {
 	// Initializes main slider
-	$( '.sell-points .entry-list' ).bxSlider( { 
+	$( '.sell-points-entry-list' ).bxSlider( { 
 		auto: true, 
 		autoStart: true, 
 		infiniteLoop: true, 

06d6fc0516b266cc57a051195b454c55f6a9189f
h5avlov <h5avlov@abv.bg>
2020-09-02 15:51:13 +0300

    Sell-points functionality added

---
+++ assets/js/sell-points.js
@@ -0,0 +1,14 @@
+( function() {
+	// Initializes main slider
+	$( '.sell-points .entry-list' ).bxSlider( { 
+		auto: true, 
+		autoStart: true, 
+		infiniteLoop: true, 
+		mode: "vertical", 
+		adaptiveHeight: true,  
+		pause: 7000, 
+		autoHover: true, 
+		stopAutoOnClick: true 
+	} ); 
+ 
+}() );

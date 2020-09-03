c75bc34011d179caa026ad69f65a2436d6b5ea78
ellibeetherapy <55092488+h5avlov@users.noreply.github.com>
2020-09-03 17:13:43 +0300

    Destination directory changed to 'docs'

---
+++ docs/assets/js/sell-points.js
@@ -0,0 +1,15 @@
+( function() {
+	// Initializes main slider
+	$( '.sell-points-entry-list' ).bxSlider( { 
+		auto: true, 
+		autoStart: true, 
+		infiniteLoop: true, 
+		mode: "vertical", 
+		controls: false, 
+		adaptiveHeight: true,  
+		pause: 7000, 
+		autoHover: true, 
+		stopAutoOnClick: true 
+	} ); 
+ 
+}() );
